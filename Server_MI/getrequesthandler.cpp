10fc3ad04bf5e3cb9654e799bfc8fb9a1ddfda4b
DeNoctiss <stardustdragon951@gmail.com>
2020-03-15 14:14:22 +0300

    ufff

--- Server_MI/getrequesthandler.cpp
+++ Server_MI/getrequesthandler.cpp
@@ -12,7 +12,6 @@ QString GetRequestHandler::getRoutesHandler()
     QJsonArray routes;
     QSqlQuery* query = new QSqlQuery(*DB_);
     if(query->exec("select Route.id, Route.race, Route.from_, Route.to_, Date.date from Route join Date ON Route.date_id = Date.id;")){
-        int count = 1;
         while (query->next()) {
             QJsonObject route;
             route["id"]=query->value(0).toString();
@@ -21,8 +20,7 @@ QString GetRequestHandler::getRoutesHandler()
             route["to"]= query->value(3).toString();
             route["date"]=query->value(4).toString();
             routes.append(route);
-            qDebug() << count;
-            count++;
+
 
         }
     }
@@ -47,6 +45,8 @@ QString GetRequestHandler::getDataFlightHendler()
             data["total_intensity"]=query->value(3).toInt();
             data["direction"]=query->value(4).toInt();
             dataArray.append(data);
+            qDebug() << count;
+            count++;
 
         }
     }
