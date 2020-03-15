88c270a062d145999fba10672c9880dfd1268687
DeNoctiss <stardustdragon951@gmail.com>
2020-03-15 14:11:27 +0300

    ufff

--- Server_MI/getrequesthandler.cpp
+++ Server_MI/getrequesthandler.cpp
@@ -21,7 +21,9 @@ QString GetRequestHandler::getRoutesHandler()
             route["to"]= query->value(3).toString();
             route["date"]=query->value(4).toString();
             routes.append(route);
+            qDebug() << count;
             count++;
+
         }
     }
     QJsonDocument doc;
